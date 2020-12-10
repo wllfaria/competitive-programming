5ba46381582177e5feb1618ceaeebf4acad82805
Willians Faria <dev.willians.faria@gmail.com>
2020-12-10 17:30:49 -0300

    adding solved atcoder practice problem

---
+++ problems/atcoder/practice_contest/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+	int a, b, c;
+	string s;
+	cin >> a >> b >> c >> s;
+	cout << a + b + c << " " + s << endl;
+}
+
+int main() {
+	solve();
+	return 0;
+}
